117ea2399e46332bec90e0faafc4a3fe19035cef
chenaoh <cdhenao@gmail.com>
2022-10-25 12:14:38 -0300

    Se grega el Proyecyo

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int contraseña = 12345678;
+
+Console.WriteLine("Ingrese contraseña numérica de 8 dígitos:");
+int contraseñaIngresada = int.Parse(Console.ReadLine());
+
+if(contraseña == contraseñaIngresada)
+{
+    Console.WriteLine("Bienvenido");
+}
+else
+{
+    Console.WriteLine("Ingrese contraseña nuevamente");
+    contraseñaIngresada = int.Parse(Console.ReadLine());
+    if (contraseña == contraseñaIngresada)
+    {
+        Console.WriteLine("Bienvenido");
+    }
+    else
+    {
+        Console.WriteLine("Reiniciar el sistema");
+    }
+}
